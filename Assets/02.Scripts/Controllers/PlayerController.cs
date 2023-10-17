2c9c6f03c9cf5acfbc5db5a04ba618256dca979b
dollbride <polly.dollbride@gmail.com>
2023-10-17 17:29:57 +0900

    Create PlayerController

---
+++ Assets/02.Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Platformer.Controllers
+{
+    public class PlayerController : CharacterController
+    {
+        public override float horizontal => Input.GetAxisRaw("Horizontal");
+
+        public override float vertical => Input.GetAxisRaw("Vertical");
+    }
+}
